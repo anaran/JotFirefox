a2f476b05eaf4713f76acee7cfda7a4b587aa02f
Adrian Aichner <adrian.aichner@gmail.com>
2014-01-18 18:09:21 +0100

    ESLint and Format JS in orion.

--- Snapper/lib/main.js
+++ Snapper/lib/main.js
@@ -1,33 +1,35 @@
+'use strict';
+/*global require: false */
 var cm = require("sdk/context-menu");
 var selection = require("sdk/selection");
 var notifications = require("sdk/notifications");
 var textSelections = [];
 
 if (!selection.isContiguous) {
-  for (var subselection in selection) {
-     textSelections.push(subselection.text);
-  }
+    for (var subselection in selection) {
+        textSelections.push(subselection.text);
+    }
 } else {
     textSelections.push(selection.text);
 }
 
 cm.Item({
-  label: "Snap Information",
-  // context: /* New in Firefox 29 */ PredicateContext(function(context) { return true; }),
-  contentScript: 'self.on("click", function (node, data) {' +
-                 '  self.postMessage({date: Date.now(), url: document.URL, "node": node, "data": data });' +
-                 '});',
-  onMessage: function (message) {
-    // /* window is not defined */ window.alert(message);
-notifications.notify({
-  title: "Snapped Information",
-  text: JSON.stringify(message)
-  // data: "did gyre and gimble in the wabe",
-  /* onClick: function (data) {
+    label: "Snap Information",
+    // context: /* New in Firefox 29 */ PredicateContext(function(context) { return true; }),
+    contentScript: 'self.on("click", function (node, data) {' +
+        '  self.postMessage({date: Date.now(), url: document.URL, "node": node, "data": data });' +
+        '});',
+    onMessage: function(message) {
+        // /* window is not defined */ window.alert(message);
+        notifications.notify({
+            title: "Snapped Information",
+            text: JSON.stringify(message)
+            // data: "did gyre and gimble in the wabe",
+            /* onClick: function (data) {
     console.log(data);
     // console.log(this.data) would produce the same result.
   } */
-});
-    // console.log('onMessage', message);
-  }
+        });
+        // console.log('onMessage', message);
+    }
 });
