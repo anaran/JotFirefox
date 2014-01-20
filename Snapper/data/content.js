7ecd1589b903672fea7e0403cd3511f55314aa63
Adrian Aichner <adrian.aichner@gmail.com>
2014-01-20 02:23:39 +0100

    Positioning of snapperDiv works for contextment (Desktop) and touch (Mobile).

--- Snapper/data/content.js
+++ Snapper/data/content.js
@@ -12,39 +12,43 @@
 self.port.on("welcome", function(tag) {
     console.log(tag);
     var displayInformation = function(event) {
+        var myX = (event.targetTouches) ? event.targetTouches[event.targetTouches.length - 1].clientX : event.clientX;
+        var myY = (event.targetTouches) ? event.targetTouches[event.targetTouches.length - 1].clientY : event.clientY;
+//        var myX = (event.targetTouches && event.targetTouches.length === 1) ? event.targetTouches[event.targetTouches.length - 1].clientX : event.clientX;
+//        var myY = (event.targetTouches && event.targetTouches.length === 1) ? event.targetTouches[event.targetTouches.length - 1].clientY : event.clientY;
         console.log(event.type, event.target, event.detail);
         // document.addEventListener('contextmenu', function(event) {
         // document.addEventListener('click', function(event) {
         var myDocument = document;
-        var autosaveIndicator = myDocument.createElement('div');
-        autosaveIndicator.style.position = 'fixed';
-        autosaveIndicator.style.top = '20px';
-        autosaveIndicator.style.left = '20px';
-        //        autosaveIndicator.style.bottom = '1em';
-        //        autosaveIndicator.style.right = '1em';
-        autosaveIndicator.style.backgroundColor = 'white';
-        autosaveIndicator.style.border = '1px dashed';
-        // autosaveIndicator.style.transition = 'opacity 1s 0s';
-
-        var downloadLink = myDocument.createElement('pre');
-        downloadLink.textContent = document.URL + '\n' + document.title + '\n';
-        autosaveIndicator.appendChild(downloadLink);
-
-        var close = autosaveIndicator.appendChild(myDocument.createElement('span'));
-        close.textContent = "[x]";
+        var snapperDiv = myDocument.createElement('div');
+        snapperDiv.style.position = 'fixed';
+        if (true && document.documentElement && (myX > document.documentElement.clientWidth / 2)) {
+            snapperDiv.style.right = (document.documentElement.clientWidth - myX) + 'px';
+        } else {
+            snapperDiv.style.left = myX + 'px';
+        }
+        if (true && document.documentElement && (myY > document.documentElement.clientHeight / 2)) {
+            snapperDiv.style.bottom = (document.documentElement.clientHeight - myY) + 'px';
+        } else {
+            snapperDiv.style.top = myY + 'px';
+        }
+        snapperDiv.style.backgroundColor = 'white';
+        snapperDiv.style.padding = '2px';
+        snapperDiv.style.border = '1px dashed';
+        var snapperPre = myDocument.createElement('pre');
+        var selection = document.getSelection().toString();
+        snapperPre.textContent = document.title + (selection ? '\n\n' + selection : '') + '\n\n' + document.URL + '\n';
+        snapperDiv.appendChild(snapperPre);
+        var close = snapperDiv.appendChild(myDocument.createElement('input'));
+        close.value = "Close";
+        close.type = 'button';
         close.addEventListener('click', function(event) {
             event.preventDefault();
-            document.body.removeChild(autosaveIndicator);
+            event.stopPropagation();
+            document.body.removeChild(snapperDiv);
         }, false);
-        myDocument.body.appendChild(autosaveIndicator);
-        //    document.addEventListener('touchstart', function(event) {
-        //        //    self.on('contextmenu', function(event) {
-        //        console.log(event.type, event.target, event.detail);
-        //        self.port.emit('contextmenu', event.type);
-        //        }, false);
-
-        //    window.alert(JSON.stringify({ event.type, event.target, event.detail }));
-    }
+        myDocument.body.appendChild(snapperDiv);
+    };
     document.addEventListener('contextmenu', displayInformation, false);
     document.addEventListener('touchend', displayInformation, false);
     self.port.emit('hello', 'goodbye');
