82261ae7b42a5e44689230a11559ca95d7a0a9e6
Adrian Aichner <adrian.aichner@gmail.com>
2014-01-22 00:53:29 +0100

    Menu entry "Snapper" produces alert in Desktop, toast in android firefox.

--- Snapper/data/content.js
+++ Snapper/data/content.js
@@ -9,49 +9,63 @@
 //            "data": data
 //    });
 //};
+var displayInformation = function( /*event*/ ) {
+    //        var myX = (event.targetTouches) ? event.targetTouches[event.targetTouches.length - 1].clientX : event.clientX;
+    //        var myY = (event.targetTouches) ? event.targetTouches[event.targetTouches.length - 1].clientY : event.clientY;
+    //        console.log(event.type, event.target, event.detail);
+    var myDocument = document;
+    var snapperDiv = myDocument.createElement('div');
+    snapperDiv.style.position = 'fixed';
+    //        if (true && document.documentElement && (myX > document.documentElement.clientWidth / 2)) {
+    //            snapperDiv.style.right = (document.documentElement.clientWidth - myX) + 'px';
+    //        } else {
+    //            snapperDiv.style.left = myX + 'px';
+    //        }
+    //        if (true && document.documentElement && (myY > document.documentElement.clientHeight / 2)) {
+    //            snapperDiv.style.bottom = (document.documentElement.clientHeight - myY) + 'px';
+    //        } else {
+    //            snapperDiv.style.top = myY + 'px';
+    //        }
+    snapperDiv.style.left = '2em';
+    snapperDiv.style.top = '2em';
+    snapperDiv.style.backgroundColor = 'white';
+    snapperDiv.style.padding = '2px';
+    snapperDiv.style.border = '1px dashed';
+    var snapperPre = myDocument.createElement('pre');
+    var selection = document.getSelection().toString();
+    snapperPre.textContent = document.title + (selection ? '\n\n' + selection : '') + '\n\n' + document.URL + '\n';
+    snapperDiv.appendChild(snapperPre);
+    var close = snapperDiv.appendChild(myDocument.createElement('input'));
+    close.value = "Close";
+    close.type = 'button';
+    close.addEventListener('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        document.body.removeChild(snapperDiv);
+    }, false);
+    myDocument.body.appendChild(snapperDiv);
+};
 self.port.on("welcome", function(tag) {
     console.log(tag);
-    var displayInformation = function(event) {
-        var myX = (event.targetTouches) ? event.targetTouches[event.targetTouches.length - 1].clientX : event.clientX;
-        var myY = (event.targetTouches) ? event.targetTouches[event.targetTouches.length - 1].clientY : event.clientY;
-//        var myX = (event.targetTouches && event.targetTouches.length === 1) ? event.targetTouches[event.targetTouches.length - 1].clientX : event.clientX;
-//        var myY = (event.targetTouches && event.targetTouches.length === 1) ? event.targetTouches[event.targetTouches.length - 1].clientY : event.clientY;
-        console.log(event.type, event.target, event.detail);
-        // document.addEventListener('contextmenu', function(event) {
-        // document.addEventListener('click', function(event) {
-        var myDocument = document;
-        var snapperDiv = myDocument.createElement('div');
-        snapperDiv.style.position = 'fixed';
-        if (true && document.documentElement && (myX > document.documentElement.clientWidth / 2)) {
-            snapperDiv.style.right = (document.documentElement.clientWidth - myX) + 'px';
-        } else {
-            snapperDiv.style.left = myX + 'px';
-        }
-        if (true && document.documentElement && (myY > document.documentElement.clientHeight / 2)) {
-            snapperDiv.style.bottom = (document.documentElement.clientHeight - myY) + 'px';
-        } else {
-            snapperDiv.style.top = myY + 'px';
-        }
-        snapperDiv.style.backgroundColor = 'white';
-        snapperDiv.style.padding = '2px';
-        snapperDiv.style.border = '1px dashed';
-        var snapperPre = myDocument.createElement('pre');
-        var selection = document.getSelection().toString();
-        snapperPre.textContent = document.title + (selection ? '\n\n' + selection : '') + '\n\n' + document.URL + '\n';
-        snapperDiv.appendChild(snapperPre);
-        var close = snapperDiv.appendChild(myDocument.createElement('input'));
-        close.value = "Close";
-        close.type = 'button';
-        close.addEventListener('click', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            document.body.removeChild(snapperDiv);
-        }, false);
-        myDocument.body.appendChild(snapperDiv);
-    };
-    document.addEventListener('contextmenu', displayInformation, false);
-    document.addEventListener('touchend', displayInformation, false);
+    //    (function() {
+    // See https://developer.mozilla.org/en-US/Add-ons/Firefox_for_Android/Initialization_and_Cleanup?redirectlocale=en-US&redirectslug=Extensions%2FMobile%2FInitialization_and_Cleanup
+    //        var Cc = Components.classes;
+    //        var Ci = Components.interfaces;
+    //        var wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+    //
+    //        // Load into any existing windows
+    //        var windows = wm.getEnumerator("navigator:browser");
+    //        var domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    // if ( !! domWindow && domWindow.NativeWindow) {
+    //    document.addEventListener('contextmenu', displayInformation, false);
+    //    document.addEventListener('touchend', displayInformation, false);
+    // })();
     self.port.emit('hello', 'goodbye');
+    self.port.on("display", function(tag) {
+        if (tag === "information") {
+            displayInformation();
+        }
+    });
 });
 // self.on("click", postInformation);
 // setInterval(postInformation, 5000);
--- Snapper/lib/main.js
+++ Snapper/lib/main.js
@@ -1,5 +1,5 @@
 // 'use strict';
-/*global require: false */
+/*global require: false, console: false, gBrowser: false */
 // var cm = require("sdk/context-menu");
 var data = require("sdk/self").data;
 // var selection = require("sdk/selection");
@@ -8,44 +8,62 @@ var pageMod = require("sdk/page-mod");
 pageMod.PageMod({
     include: ['*'],
     contentScriptWhen: 'end',
-     attachTo: ["existing", "top"],
-  onAttach: function(worker) {
-//    worker.port.emit("getElements", tag);
-    worker.port.emit("welcome", 'content');
-    worker.port.on("hello", function(reply) {
-        console.log(reply);
-    });
-    worker.port.on("contextmenu", function(reply) {
-        console.log("contextmenu", reply);
-    });
-  },
+    attachTo: ["existing", "top"],
+    onAttach: function(worker) {
+        //    worker.port.emit("getElements", tag);
+        worker.port.emit("welcome", 'content');
+        worker.port.on("hello", function(reply) {
+            console.log(reply);
+        });
+        worker.port.on("contextmenu", function(reply) {
+            console.log("contextmenu", reply);
+        });
+    },
     contentScriptFile: data.url("content.js")
 });
-//var textSelections = [];
-//
-//if (!selection.isContiguous) {
-//    for (var subselection in selection) {
-//        textSelections.push(subselection.text);
-//    }
-//} else {
-//    textSelections.push(selection.text);
-//}
+// See https://blog.mozilla.org/addons/2013/06/13/jetpack-fennec-and-nativewindow
+// get a global window reference
+const utils = require('sdk/window/utils');
+const recent = utils.getMostRecentBrowserWindow();
+if (recent.NativeWindow) {
+    // Add a menu item
+    // var menuID = recent.NativeWindow.menu.add(label, icon, callback);
+    var menuID = recent.NativeWindow.menu.add('Snapper', null, function() {
+        var activeTab = require("sdk/tabs").activeTab;
+        // var activeTab = undefined;
+        // recent.NativeWindow.toast.show("Snap!", "long");
+        recent.NativeWindow.toast.show("Snap!\n" + (activeTab ? (activeTab.title + '\n' + activeTab.url) : ('No Title' + '\n' + 'No URL')), "long");
+        // activeTab.attach({
+        //       contentScript:
+        //         'displayInformation();'
+        //   });
+        // Show a doorhanger
+        // recent.NativeWindow.doorhanger.show(message, value, buttons, tabID, options);
 
-//cm.Item({
-//    label: "Snap Information",
-//    // context: /* New in Firefox 29 */ PredicateContext(function(context) { return true; }),
-//    contentScriptFile: data.url('context-menu.js'),
-//    onMessage: function(message) {
-//        // /* window is not defined */ window.alert(message);
-//        notifications.notify({
-//            title: "Snapped Information",
-//            text: JSON.stringify(message)
-//            // data: "did gyre and gimble in the wabe",
-//            /* onClick: function (data) {
-//    console.log(data);
-//    // console.log(this.data) would produce the same result.
-//  } */
-//        });
-//        // console.log('onMessage', message);
-//    }
-//});
+        // add a context menu item
+        // var contextMenuID = recent.NativeWindow.contextmenu.add(label, selector, callback);
+
+        // show a 'toast' notification
+        // recent.NativeWindow.toast.show(message, duration);
+        // See https://gist.github.com/canuckistani/7559147/raw/d23d43f2a8a1cfafb14dda44452278aa77e45cd6/tools-menu-hack.js
+        //    require("tools-menu-hack").addMenuItem('Snapper', function() {
+        //        worker.port.emit("display", 'information');
+        //    });
+    });
+} else {
+    var menuitem = require("menuitems").Menuitem({
+        id: "snapper",
+        menuid: "menu_ToolsPopup",
+        label: "Snapper",
+        accesskey: 'n',
+        onCommand: function() {
+            var activeTab = require("sdk/tabs").activeTab;
+            // var activeTab = undefined;
+            // recent.NativeWindow.toast.show("Snap!", "long");
+            recent.alert("Snap!\n" + (activeTab ? (activeTab.title + '\n' + activeTab.url) : ('No Title' + '\n' + 'No URL')), "long");
+            //            worker.port.emit("display", 'information');
+            //            console.log("clicked");
+        },
+        // insertbefore: "menu_pageInfo"
+    });
+}
