bc61dc9eb4853b7dc6521d7ea7795fbcfb793d0f
Adrian Aichner <adrian.aichner@gmail.com>
2014-01-31 20:39:02 +0100

    Replace DEBUG option with consoleLogLevel, add user formats 1 and 2.
    Add notifications for Adding and Downloading (works fine on WIndows and Android).
    Only add snap when the exact stringified object is not yet in simple-storage.

--- Snapper/lib/main.js
+++ Snapper/lib/main.js
@@ -3,18 +3,19 @@
 
 let sp = require('sdk/simple-prefs');
 let self = require('sdk/self');
+let notifications = require("sdk/notifications");
 var data = self.data;
 
-var DEBUG = sp.prefs.DEBUG;
+var consoleLogLevel = sp.prefs.consoleLogLevel;
 var DATEFORMAT = sp.prefs.DATEFORMAT;
 var INFOFORMAT = sp.prefs.INFOFORMAT;
 
-sp.on('DEBUG', function() {
-    DEBUG = sp.prefs.DEBUG;
+sp.on('consoleLogLevel', function() {
+    consoleLogLevel = sp.prefs.consoleLogLevel;
     var name = "extensions." + self.id + ".sdk.console.logLevel";
     // TODO Using error to make sure message will always be visible -- not ideal.
-    console.error('Setting log level for ' + self.name + ' version ' + self.version + ' to ' + (DEBUG ? 'all' : 'error'));
-    require("sdk/preferences/service").set(name, (DEBUG ? 'all' : 'error'));
+    console.error('Setting log level for ' + self.name + ' version ' + self.version + ' to ' + consoleLogLevel);
+    require("sdk/preferences/service").set(name, consoleLogLevel);
 });
 //sp.on('DATE_FORMAT', function() {
 //    DATE_FORMAT = sp.prefs.DATE_FORMAT;
@@ -48,13 +49,34 @@ var openSnapperTab = function(data) {
                 if (!snapperStorage.storage.entries) {
                     snapperStorage.storage.entries = [];
                 }
+                console.log(snapperStorage.storage.entries);
+                for (var i = 0, len = snapperStorage.storage.entries.length; i < len; i++) {
+                    if (JSON.stringify(snapperStorage.storage.entries[i]) === JSON.stringify(data)) {
+                        notifications.notify({
+                            text: 'Already saved (skipping) ' + JSON.stringify(data)
+                        });
+                        return;
+                    }
+                }
+                notifications.notify({
+                    text: 'Saving ' + JSON.stringify(data)
+                });
                 snapperStorage.storage.entries.push(data);
                 console.log(snapperStorage.storage.entries);
             });
             worker.port.on('download', function(data) {
+                var filename = self.name + snapperStorage.storage.entries.length + '@' + Date.now() + '.txt';
                 console.log('snapperStorage.quotaUsage:', snapperStorage.quotaUsage);
-            worker.port.emit("entries", {entries: snapperStorage.storage.entries,
-            filename: self.name + '@' + Date.now() + '.txt'});
+                console.log(JSON.stringify(snapperStorage.storage.entries));
+                if ( !! snapperStorage.storage.entries) {
+                    notifications.notify({
+                        text: 'Downloading ' + filename
+                    });
+                    worker.port.emit("entries", {
+                        entries: snapperStorage.storage.entries,
+                        filename: filename
+                    });
+                }
             });
         }
         tabs.open({
